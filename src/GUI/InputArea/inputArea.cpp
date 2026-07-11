f0dbd3855de1217d8a329fa1381537bcca8fe81d
Fasdr <saaa1997@gmail.com>
2026-07-11 15:47:17 -0400

    Added selection modifier to tablet events

--- src/GUI/InputArea/inputArea.cpp
+++ src/GUI/InputArea/inputArea.cpp
@@ -144,8 +144,6 @@ void InputArea::tabletEvent(QTabletEvent* event) {
     static bool selectionMode{};
     static Stroke stroke;
 
-    qDebug() << "Event: " << event->type() << " Buttons: " << event->buttons();
-
     auto actualEvent{event->type()};
     bool selecting{(event->buttons() & Qt::MiddleButton) != 0};
     if (selecting) {
